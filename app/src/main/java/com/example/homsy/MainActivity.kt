5a267e9a7a84bb165bbcb6c168cd73d1697b63fd
OlgaVikultseva <olga.vikultseva@exyte.com>
2022-01-11 15:24:57 +0700

    Change the appearance of System UI elements.

--- app/src/main/java/com/example/homsy/MainActivity.kt
+++ app/src/main/java/com/example/homsy/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.homsy
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homsy.databinding.ActivityMainBinding
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        transparentStatusAndNavigationBar()
+    }
+
+    private fun AppCompatActivity.transparentStatusAndNavigationBar() {
+
+        val statusAndNavigationBarColor = MaterialColors.getColor(this, R.attr.statusAndNavigationBarColor, Color.BLACK)
+
+        val winParams = window.attributes
+        winParams.flags =
+            winParams.flags and (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION).inv()
+
+        val systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        window.decorView.systemUiVisibility = systemUiVisibility
+
+        window.statusBarColor = statusAndNavigationBarColor
+        window.navigationBarColor = statusAndNavigationBarColor
+        window.attributes = winParams
     }
 }
